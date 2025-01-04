600fb6eb42c3e52126f72edf01b4280d37735053
EsrefDesign <esrefdesign@gmail.com>
2025-01-04 13:19:02 +0300

    added Crud enpoints

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -40,6 +40,47 @@ router.post('/add',async(req,res)=>{
     }
 })
 
+
+router.post('/update',async(req,res)=>{
+    let body = req.body
+    try {
+
+        if (!body._id)  throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST,"Validation Error!","name field must be")
+
+        let updates = {}
+        
+        if(body.name) updates.name=body.name;
+        if(typeof body.is_active === 'boolean') updates.is_active=body.is_active;
+
+        await Categories.updateOne({_id:body._id},updates);
+
+        res.json(Response.successResponse({succses: true}));
+        
+
+    } catch (error) {
+        let errorResponse=Response.errorResponse(error)
+        res.status(errorResponse.code).json(errorResponse)
+    }
+})
+
+
+router.post('/delete',async(req,res)=>{
+    let body = req.body
+    try {
+
+        if (!body._id)  throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST,"Validation Error!","name field must be")
+        
+        await Categories.deleteOne({_id:body._id});
+        
+        res.json(Response.successResponse({succses: true}));
+        
+
+    } catch (error) {
+        let errorResponse=Response.errorResponse(error)
+        res.status(errorResponse.code).json(errorResponse)
+    }
+})
+
 module.exports = router;
 
 
